725e987a1bd8db345639547a836d2bd450d4b0c3
ronys <ronys@3f901d5b-7c5d-4663-935e-8925751b605d>
2008-11-13 21:11:10 +0000

    Bring all code under Artistic Licence 2.0 and note David as the copyright owner.
    git-svn-id: svn://svn.code.sf.net/p/jpwsafe/code/trunk/PasswordSafeSWT@244 3f901d5b-7c5d-4663-935e-8925751b605d

--- src/org/pwsafe/passwordsafeswt/model/PasswordTableSorter.java
+++ src/org/pwsafe/passwordsafeswt/model/PasswordTableSorter.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2008 David Muller <roxon@users.sourceforge.net>.
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
 package org.pwsafe.passwordsafeswt.model;
 
 import org.eclipse.jface.viewers.Viewer;
