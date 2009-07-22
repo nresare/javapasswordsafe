609a7835587b96844e5a884eb1b493d89d026a76
roxon <roxon@3f901d5b-7c5d-4663-935e-8925751b605d>
2009-07-22 20:44:07 +0000

    set svn props
    git-svn-id: svn://svn.code.sf.net/p/jpwsafe/code/trunk@399 3f901d5b-7c5d-4663-935e-8925751b605d

--- PasswordSafeSWT/src/org/pwsafe/passwordsafeswt/action/CopyURLAction.java
+++ PasswordSafeSWT/src/org/pwsafe/passwordsafeswt/action/CopyURLAction.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2008-2009 David Muller <roxon@users.sourceforge.net>, Tim Hughes.
+ * $Id$
+ * Copyright (c) 2008-2009 David Muller <roxon@users.sourceforge.net>
  * All rights reserved. Use of the code is allowed under the
  * Artistic License 2.0 terms, as specified in the LICENSE file
  * distributed with this code, or available from
