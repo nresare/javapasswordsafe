610cbc542479a2bcd87f859520393a68bbaf5492
roxon <roxon@3f901d5b-7c5d-4663-935e-8925751b605d>
2009-11-26 21:37:36 +0000

    Fix Trac #10 Improve SHA256Pws.digest Performance
    git-svn-id: svn://svn.code.sf.net/p/jpwsafe/code/trunk@434 3f901d5b-7c5d-4663-935e-8925751b605d

--- PasswordSafeLib/test/org/pwsafe/lib/crypto/SHA256PwsTest.java
+++ PasswordSafeLib/test/org/pwsafe/lib/crypto/SHA256PwsTest.java
@@ -7,9 +7,9 @@
  */
 package org.pwsafe.lib.crypto;
 
-import org.pwsafe.lib.Util;
-
 import junit.framework.TestCase;
+
+import org.pwsafe.lib.Util;
 
 /**
  * Test SHA256.
@@ -21,7 +21,8 @@ public class SHA256PwsTest extends TestCase {
 	public void testDigest() {
 
 		String data = "abc";
-		byte[] digest = SHA256Pws.digest(data.getBytes());
+		SHA256Pws hasher = new SHA256Pws();
+		byte[] digest = hasher.digest(data.getBytes());
 
 		String result = Util.bytesToHex(digest);
 		assertEquals("ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad", result);
