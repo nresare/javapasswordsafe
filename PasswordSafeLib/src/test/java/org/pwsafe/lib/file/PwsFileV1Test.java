e0d8b2a0a3ba1e02e8f98778dcd187fa7e4f7a79
Noa Resare <noa@resare.com>
2016-08-19 22:51:03 +0200

    PwsFileV1Test cleanup
    * Moved 3 tests into a sigle method, since they where interdependent
    * Generate temporary filename
    * Make sure the temp file is deleted on exit, regardless of success

--- PasswordSafeLib/src/test/java/org/pwsafe/lib/file/PwsFileV1Test.java
+++ PasswordSafeLib/src/test/java/org/pwsafe/lib/file/PwsFileV1Test.java
@@ -13,92 +13,80 @@ import junit.framework.TestCase;
 
 import org.pwsafe.util.FileConverter;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 /**
  *
  */
 public class PwsFileV1Test extends TestCase {
-	private static final String V1_FILE_NAME = "V1 New File.dat";
 	private static final String PASSPHRASE = "passphrase";
 
 	/**
-	 * Tests that the library can create a version 1 database.
+	 * Tests that the library can create a version 1 database, read from the created file and
+	 * round trip convert to V2.
 	 */
-	public void testCreateV1File() throws Exception {
-		PwsRecord rec = null;
-		final PwsFile v1File = new PwsFileV1();
+	public void testV1File() throws Exception {
+		Path tmpFile = Files.createTempFile("javapasswordsafe_v1", ".dat");
+		assertTrue(tmpFile.toFile().delete());
+		String v1FileName = tmpFile.toString();
 
-		v1File.setStorage(new PwsFileStorage(V1_FILE_NAME));
-		v1File.setPassphrase(new StringBuilder(PASSPHRASE));
+		try {
+			PwsRecord rec = null;
+			final PwsFile v1File = new PwsFileV1();
 
-		rec = v1File.newRecord();
+			v1File.setStorage(new PwsFileStorage(v1FileName));
+			v1File.setPassphrase(new StringBuilder(PASSPHRASE));
 
-		rec.setField(new PwsStringField(PwsRecordV1.TITLE, "Entry number 1"));
-		rec.setField(new PwsStringField(PwsRecordV1.PASSWORD, "Password 1"));
+			rec = v1File.newRecord();
 
-		v1File.add(rec);
+			rec.setField(new PwsStringField(PwsRecordV1.TITLE, "Entry number 1"));
+			rec.setField(new PwsStringField(PwsRecordV1.PASSWORD, "Password 1"));
 
-		rec = v1File.newRecord();
+			v1File.add(rec);
 
-		rec.setField(new PwsStringField(PwsRecordV1.TITLE, "Entry number 2"));
-		rec.setField(new PwsStringField(PwsRecordV1.PASSWORD, "Password 2"));
-		rec.setField(new PwsStringField(PwsRecordV1.USERNAME, "Username 2"));
-		rec.setField(new PwsStringField(PwsRecordV1.NOTES, "Notes line 1\r\nNotes line 2"));
+			rec = v1File.newRecord();
 
-		v1File.add(rec);
+			rec.setField(new PwsStringField(PwsRecordV1.TITLE, "Entry number 2"));
+			rec.setField(new PwsStringField(PwsRecordV1.PASSWORD, "Password 2"));
+			rec.setField(new PwsStringField(PwsRecordV1.USERNAME, "Username 2"));
+			rec.setField(new PwsStringField(PwsRecordV1.NOTES, "Notes line 1\r\nNotes line 2"));
 
-		assertTrue("Modified flag is not TRUE", v1File.isModified());
-		assertEquals("Record count is not = 2", 2, v1File.getRecordCount());
+			v1File.add(rec);
 
-		v1File.save();
+			assertTrue("Modified flag is not TRUE", v1File.isModified());
+			assertEquals("Record count is not = 2", 2, v1File.getRecordCount());
 
-		assertTrue("Modified flag is not FALSE", !v1File.isModified());
+			v1File.save();
 
-		v1File.dispose();
-		assertEquals(null, v1File.getPassphrase());
+			assertTrue("Modified flag is not FALSE", !v1File.isModified());
 
+			v1File.dispose();
+			assertEquals(null, v1File.getPassphrase());
+
+			PwsFile anotherV1File = new PwsFileV1(new PwsFileStorage(v1FileName), PASSPHRASE);
+			anotherV1File.readAll();
+			anotherV1File.close();
+
+			assertEquals(2, anotherV1File.getRecordCount());
+
+
+			PwsFileV1 thirdV1File = new PwsFileV1(new PwsFileStorage(v1FileName), PASSPHRASE);
+			thirdV1File.readAll();
+			thirdV1File.close();
+
+			final PwsFileV2 newTempFile = (PwsFileV2) FileConverter.convertV1ToV2(thirdV1File);
+			assertEquals(2, newTempFile.getRecordCount());
+
+			final PwsFileV1 newFile = (PwsFileV1) FileConverter.convertV2ToV1(newTempFile);
+			assertEquals(2, newFile.getRecordCount());
+
+			assertEquals(thirdV1File.getRecord(0), newFile.getRecord(0));
+
+		} finally {
+			assertTrue(tmpFile.toFile().delete());
+		}
 	}
 
-	/**
-	 * Tests that the library can create a version 1 database.
-	 */
-	public void testReadV1File() throws Exception {
-		final PwsFile v1File = new PwsFileV1(new PwsFileStorage(V1_FILE_NAME), PASSPHRASE);
-		v1File.readAll();
-		v1File.close();
-
-		assertEquals(2, v1File.getRecordCount());
-
-	}
-
-	/**
-	 * Tests that the library can create a version 1 database.
-	 */
-	public void testConvertV1File() throws Exception {
-		final PwsFileV1 v1File = new PwsFileV1(new PwsFileStorage(V1_FILE_NAME), PASSPHRASE);
-		v1File.readAll();
-		v1File.close();
-
-		final PwsFileV2 newTempFile = (PwsFileV2) FileConverter.convertV1ToV2(v1File);
-		assertEquals(2, newTempFile.getRecordCount());
-
-		final PwsFileV1 newFile = (PwsFileV1) FileConverter.convertV2ToV1(newTempFile);
-		assertEquals(2, newFile.getRecordCount());
-
-		assertEquals(v1File.getRecord(0), newFile.getRecord(0));
-	}
-
-	// public void testProviders() throws Exception {
-	// Provider p[] = Security.getProviders();
-	// for (Provider pro : p) {
-	// System.out.println (pro);
-	// for (Enumeration e = pro.keys(); e.hasMoreElements();) {
-	// System.out.println ("\t" + e.nextElement());
-	// }
-	//
-	// }
-	// BouncyCastleProvider bcp = new BouncyCastleProvider ();
-	// System.out.println(bcp.getName());
-	// System.out.println(bcp.getInfo());
-	// System.out.println(bcp.getServices());
-	// }
 }
